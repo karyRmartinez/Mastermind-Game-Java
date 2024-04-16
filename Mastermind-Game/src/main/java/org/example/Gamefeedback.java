6cba4c55cfe5fe9e8c062ffdfcc003b996db8c6a
karyRmartinez <karymartinez@pursuit.org>
2024-04-15 21:59:46 -0400

    setters and toString implementation

--- Mastermind-Game/src/main/java/org/example/Gamefeedback.java
+++ Mastermind-Game/src/main/java/org/example/Gamefeedback.java
@@ -1,4 +1,19 @@
 package org.example;
 
 public class Gamefeedback {
+    final int correct;
+    final int correctLocation;
+
+    public Gamefeedback(int correct, int correctLocation) {
+        this.correct = correct;
+        this.correctLocation = correctLocation;
+    }
+
+    @Override
+    public String toString() {
+        return "Gamefeedback{" +
+                "correct=" + correct +
+                ", correctLocation=" + correctLocation +
+                '}';
+    }
 }
